01ded6d0306e8c62a15ac2bca3f3fd5f51bb787a
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-07-28 11:17:11 +0600

    just an update

--- First_lab_linear_search.cpp
+++ First_lab_linear_search.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 int main(){
     int n;
+    cout<<"Enter the size of array"<<endl;
     cin>>n;
     int ar[n];
+    cout<<"Enter the elements of array"<<endl;
     for(int i = 0;i<n;i++){
             cin>>ar[i];
     }
     int target;
+    cout<<"Enter the target"<<endl;
     cin>>target;
     int p = -1;
     for(int i = 0;i<n;i++){
